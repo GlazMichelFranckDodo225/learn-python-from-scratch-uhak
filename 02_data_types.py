8d291e847f7c9c5a82fa9bdb2d15fcf3a987e165
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-08-27 13:50:11 +0200

    Variables and Data Types in Python

---
+++ 02_data_types.py
@@ -0,0 +1,29 @@
+# Python supports several built-in data types:
+# Integer (int): Whole numbers, e.g., 5, -10, 0
+# Float (float): Decimal numbers, e.g., 3.14, -0.001, 2.0
+# String (str): Textual data, e.g., "Hello, World!", 'Python'
+# Boolean (bool): Represents True or False values
+# List (list): Ordered collection of items, e.g., [1, 2, 3], 
+#   ['apple', 'banana', 'cherry']
+# Tuple (tuple): Immutable ordered collection of items, e.g., (1, 2, 3), 
+#   ('apple', 'banana', 'cherry')
+# Dictionary (dict): Unordered collection of key-value pairs, e.g., 
+# {'name': 'Alice', 'age': 30} 
+# Set (set): Unordered collection of unique items, e.g., {1, 2, 3}, 
+#   {'apple', 'banana', 'cherry'}
+
+age = 3
+print(age)
+print(type(age))
+
+name = "Harry"
+print(name)
+print(type(name))
+
+price = 4.55
+print(price)
+print(type(price))
+
+is_completed = False
+print(is_completed)
+print(type(is_completed))
---
+++ 03_VARIABLES/01_basics.py
@@ -0,0 +1,19 @@
+"""
+In Python, variables are used to store information 
+that can be referenced and manipulated in a program.
+A variable is created the moment you first assign a 
+value to it using the assignment operator (=).
+"""
+age = 34 # Integer
+name = "Harry" # String
+price = 4.55 # Float
+# Rules of defining variables in Python:
+# 1. Variable names must start with a letter or an underscore (_).
+# 2. Variable names can only contain letters, numbers, and underscores.
+# 3. Variable names are case-sensitive (age and Age are different variables).
+# 4. Variable names cannot be a reserved keyword in Python (like if, else, while, etc.).
+# 34age = 34 # Invalid variable name because it starts with a number
+# a$$ge = 34 # Invalid variable name because it contains special characters
+_age = 34 # Valid variable name
+_nice_variable = 34 # Valid variable name
+a_b_c_7 = "John Doe" # Valid variable name
